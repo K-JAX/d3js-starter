3859f2ee88f29b6caa4624df4b03c3fea9e1d80d
Kevin Garubba <kevingarubba@gmail.com>
2022-10-29 22:38:23 -0400

    Removed handlebar templating. Added vite. Styled it a little bit.

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+import "./style.css";
+
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 import axios from "https://cdn.skypack.dev/axios";
 
-const dataSet = async function getData() {
-	return await axios.get("/api/data");
-};
+// const dataSet = async function getData() {
+// 	return await axios.get("/api/data");
+// };
+
+// const dataSet = [100, 50, 300, 40, 350, 250];
 async function drawChart() {
-	const data = await dataSet();
+	// const data = await dataSet();
+	let data = { data: [] };
+	data.data = [100, 50, 300, 40, 350, 250];
 	const svgWidth = 500;
 	const svgHeight = 500;
 	const barPadding = 5;
